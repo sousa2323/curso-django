fc7f34ab60eb9f91705520d77257be0f210cab92
sousa2323 <sousanot.23@gmail.com>
2022-03-20 14:09:27 -0300

    removido contatoe sbre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,9 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'João'
     })
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
