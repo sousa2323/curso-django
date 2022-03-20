59d4a8098dbff02dd109f1d8529a42450a3e9c86
sousa2323 <sousanot.23@gmail.com>
2022-03-20 13:52:11 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'João'
+    })
 
 def sobre(request):
     return HttpResponse('Sobre')
