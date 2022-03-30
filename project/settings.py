db5cf43468b121e086dad4a69a73ff1ec7c99fb1
sousa2323 <sousanot.23@gmail.com>
2022-03-29 21:17:27 -0300

    adicionando static

--- project/settings.py
+++ project/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_FILES = [
+STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
