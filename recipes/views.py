73dba520e458dbf1d32bdfe8ee3f9a7147efacba
sousa2323 <sousanot.23@gmail.com>
2022-03-31 01:16:35 -0300

    adicionando recip-view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'João'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'João'
+    })    
