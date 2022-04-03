a9bceee70bb6f01a296d8b74f6b602015760d3cb
sousa2323 <sousanot.23@gmail.com>
2022-04-02 21:52:21 -0300

    mexendo com 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
+
 from recipes.models import Recipe
 
 
@@ -11,14 +13,21 @@ def home(request):
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id,
         is_publish=True
     ).order_by('-id')
+
+    if not recipes:
+        raise Http404('No recipes found 😁')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })    
+
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
