374f9903e9b9baae0ea5683529c822ef3072ccbb
sousa2323 <sousanot.23@gmail.com>
2022-04-03 15:32:22 -0300

    introduzindo get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
-from django.http import Http404
 
 from recipes.models import Recipe
 
@@ -15,17 +14,15 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_publish=True
-    ).order_by('-id')
-
-    if not recipes:
-        raise Http404('No recipes found 😁')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_publish=True
+    ).order_by('-id'))    
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name} - Category | '
+        'title': f'{recipes[0].category.name} - Category | '
     })    
 
 
