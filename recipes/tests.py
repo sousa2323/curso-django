4739c152d408fb4e7f4007e675b124472843464f
sousa2323 <sousanot.23@gmail.com>
2022-04-06 21:17:18 -0300

    criando conf de test e testando algo

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeURLsTest(TestCase): # noqa: E302 E261
+    def test_the_pytest_is_ok(self):
+        variavel = '123456' # noqa: F841 E261
+        print('Olá mundo')
+        assert 1 == 1, '1 é igual a 1'
