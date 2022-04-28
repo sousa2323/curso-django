e7cff0cd254098f8e811e6a9eef6cb9062c965a2
sousa2323 <sousanot.23@gmail.com>
2022-04-28 20:14:09 -0300

    modificação

--- recipes/tests/test_recipe_models_recipe.py
+++ recipes/tests/test_recipe_models_recipe.py
@@ -8,13 +8,6 @@ class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
-
-    def test_recipe_title_raises_error_if_title_has_than_65_chars(self):
-        self.recipe.title = 'A' * 70
-
-        with  self.assertRaises(ValidationError):
-            self.recipe.full_clean() # Aqui a validação ocorre
-
     @parameterized.expand([
         ('title', 65),
         ('description', 165),
